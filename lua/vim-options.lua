b8a26a6fc6e84b364f9b2cbd1b9a603fcf767493
Nguyen Huu Tuan <tuannhhe172829@fpt.edu.vn>
2025-02-16 15:42:10 +0700

    #added: golang, lsp-config.lua: golang, options.lua: Shift+NN for <Esc>, <leader>+ff for telescope

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,6 +14,8 @@ vim.opt.incsearch = true
 -- move selected lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("i", "NN", "<Esc>")
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
 -- paste over highlight word
 vim.keymap.set("x", "<leader>p", '"_dP')
 vim.opt.colorcolumn = "94"
