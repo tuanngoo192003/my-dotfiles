b75e898d30a6adf59b020f0a70e30a087b3ada18
tuanngoo192003 <tuanngoo192003@DESKTOP-TUE1MKT.>
2025-03-05 16:38:31 +0000

    #updated: catppuccin.lua; #added: vim-dadbod

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,14 +1,24 @@
+-- return {
+--   "2giosangmitom/nightfall.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {}, -- Add custom configuration here
+--   config = function(_, opts)
+--     require("nightfall").setup(opts)
+--     vim.cmd("colorscheme nightfall") -- Choose from: nightfall, deeper-night, maron, nord
+--   end,
+-- }
 return {
   "catppuccin/nvim",
   name = "catppuccin",
   config = function()
   require("catppuccin").setup({
-   flavour = "auto", -- latte, frappe, macchiato, mocha
+   flavour = "macchiato", -- latte, frappe, macchiato, mocha
    background = { -- :h background
        light = "latte",
        dark = "mocha",
    },
-   transparent_background = false, -- disables setting the background color.
+   transparent_background = true, -- disables setting the background color.
    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
    dim_inactive = {
